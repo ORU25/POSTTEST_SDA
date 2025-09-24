b8d78f6105694a77f90f9c4ba423d4d6f1836937
Haykal Makhmud <haikal252005@gmail.com>
2025-09-24 20:26:54 +0800

    feat: posttest 3

---
+++ POSTTEST_3/GANJIL_2409106005.cpp
@@ -0,0 +1,305 @@
+#include <iostream>
+
+using namespace std;
+
+struct Item {
+    int id;
+    string namaItem;
+    int jumlah;
+    string type;
+};
+
+struct Node {
+    Item item;
+    Node* next;
+    Node* prev;
+};
+
+int idCounter = 0;
+
+void addItem(Node *&head, Node *&tail, string namaItem, int jumlah, string type){
+    if (head == nullptr && tail == nullptr) {
+        Node* newNode = new Node;
+        newNode->item.id = ++idCounter;
+        newNode->item.namaItem = namaItem;
+        newNode->item.jumlah = jumlah;
+        newNode->item.type = type;
+        newNode->next = nullptr;
+        newNode->prev = nullptr;
+        head = newNode;
+        tail = newNode;
+    }else{
+        Node* newNode = new Node;
+        newNode->item.id = ++idCounter;
+        newNode->item.namaItem = namaItem;
+        newNode->item.jumlah = jumlah;
+        newNode->item.type = type;
+        newNode->next = nullptr;
+        newNode->prev = tail;
+        tail->next = newNode;
+        tail = newNode;
+    }
+};
+
+void addSpesific(Node *&head, Node *&tail, string namaItem, int jumlah, string type){
+    int posisi = 5+1;
+    int mulai = 1;
+    if(head == nullptr && tail == nullptr){
+        cout << "Inventory kosong, menambahkan item sebagai item pertama." << endl;
+        addItem(head, tail, namaItem, jumlah, type);
+        return;
+    }
+    Node *temp = head;
+    while(temp != nullptr && mulai < posisi -1){
+        temp = temp->next;
+        mulai++;
+    }
+    if(temp == nullptr){
+        cout << "Posisi 5+1 melebihi panjang inventory, menambahkan item di akhir." << endl;
+        addItem(head, tail, namaItem, jumlah, type);
+    }else if(temp->next == nullptr){
+        addItem(head, tail, namaItem, jumlah, type);
+    }else {
+        Node* newNode = new Node;
+        newNode->item.id = ++idCounter;
+        newNode->item.namaItem = namaItem;
+        newNode->item.jumlah = jumlah;
+        newNode->item.type = type;
+        newNode->next = temp->next;
+        newNode->prev = temp;
+        temp->next->prev = newNode;
+        temp->next = newNode;
+    }
+};
+
+void deleteLast(Node *&head, Node *&tail){
+    Node* temp = head;
+    if(head == nullptr){
+          cout << "Inventory kosong, tidak ada yang dihapus" << endl;
+          return;
+    }
+
+    if(head->next == nullptr){
+        delete head;
+        head = nullptr;
+        tail = nullptr;
+        return;
+    }
+    
+    while(temp->next != tail){
+        temp = temp->next;
+    }
+
+    delete tail;
+    tail = temp;
+    tail->next = nullptr;
+};
+
+void useItem(Node *&head, Node *&tail, string namaItem){
+    if(head == nullptr){
+        cout << "Inventory kosong, tidak ada item yang bisa digunakan." << endl;
+        return;
+    }
+    Node* temp = head;
+    Node* prev = nullptr;
+    while(temp->item.namaItem != namaItem){
+        prev = temp;
+        temp = temp->next;
+        if(temp == nullptr){
+            cout << "Item tidak ditemukan dalam inventory." << endl;
+            return;
+        }
+    }
+
+    if(temp->item.jumlah > 0){
+        temp->item.jumlah--;
+        cout << "Item " << namaItem << " telah digunakan. Sisa jumlah: " << temp->item.jumlah << endl;
+        if(temp->item.jumlah == 0){
+            cout << "Item " << namaItem << " telah habis dan dihapus dari inventory." << endl;
+            if(prev == nullptr){
+                head = head->next;
+                delete temp;
+                if(head == nullptr){
+                    tail = nullptr;
+                }
+            }else{
+                prev->next = temp->next;
+                if(temp == tail){
+                    tail = prev;
+                }
+                delete temp;
+            }
+        }
+        
+    }else{
+        cout << "Item " << namaItem << "tidak ditemukan atau telah habis." << endl;
+    }
+};
+
+void showInventory(Node* head, Node* tail){
+    if(head == nullptr){
+        cout << "Inventory kosong." << endl;
+        return;
+    }
+    Node* temp = head;
+    int index = 1;
+    cout << "\nDaftar Item dalam Inventory:\n";
+    while(temp != nullptr){
+        cout << index << ". ID Item: " << temp->item.id << endl;
+        cout << "   Nama Item: " << temp->item.namaItem << endl;
+        cout << "   Jumlah: " << temp->item.jumlah << endl;
+        cout << "   Tipe: " << temp->item.type << endl;
+        cout << "------------------" << endl;
+        temp = temp->next;
+        index++;
+    }
+};
+
+void showInventoryTranversal(Node* head, Node* tail){
+    if(head == nullptr){
+        cout << "Inventory kosong." << endl;
+        return;
+    }
+    Node* temp = tail;
+    int index = 1;
+    cout << "\nDaftar Item dalam Inventory:\n";
+    while(temp != nullptr){
+        cout << index << ". ID Item: " << temp->item.id << endl;
+        cout << "   Nama Item: " << temp->item.namaItem << endl;
+        cout << "   Jumlah: " << temp->item.jumlah << endl;
+        cout << "   Tipe: " << temp->item.type << endl;
+        cout << "------------------" << endl;
+        temp = temp->prev;
+        index++;
+    }
+};
+
+void showDetailData(Node* head, Node* tail, int id=-1, string nama=""){
+    if(head == nullptr){
+        cout << "Inventory kosong." << endl;
+        return;
+    }
+
+    Node* temp = head;
+    if (id != -1) {
+        while (temp != nullptr && temp->item.id != id) {
+            temp = temp->next;
+        }
+    }
+    else if (!nama.empty()) {
+        while (temp != nullptr && temp->item.namaItem != nama) {
+            temp = temp->next;
+        }
+    }
+
+    if (temp == nullptr) {
+        cout << "Item tidak ditemukan dalam inventory." << endl;
+        return;
+    }
+    cout << "\nDetail Item:\n";
+    cout << "ID Item: " << temp->item.id << endl;
+    cout << "Nama Item: " << temp->item.namaItem << endl;
+    cout << "Jumlah: " << temp->item.jumlah << endl;
+    cout << "Tipe: " << temp->item.type << endl;
+};
+
+int main(){
+    Node *head = nullptr;
+    Node *tail = nullptr;
+
+    string nama;
+    string nim;
+    
+    cout << "Masukkan Nama: ";
+    getline(cin, nama);
+    cout << "Masukkan NIM: ";
+    getline(cin, nim);
+    
+    int jumlah = nim.back() - '0';
+    
+    int pilihan = -1;
+    
+    while(pilihan != 0){
+        string namaItem, type;
+
+        cout << "\n+---------------------------------------------------+\n";
+        cout << "GAME INVENTORY MANAGEMENT\n";
+        cout << nama << " - " << nim << "\n";
+        cout << "+---------------------------------------------------+\n";
+        cout << "| 1. Tambah Item Baru                               |\n";
+        cout << "| 2. Sisipkan Item                                  |\n";
+        cout << "| 3. Hapus Item Terakhir                            |\n";
+        cout << "| 4. Gunakan Item                                   |\n";
+        cout << "| 5. Tampilkan Inventory                            |\n";
+        cout << "| 6. Tampilkan Inventory Tranversal                 |\n";
+        cout << "| 7. Tampilkan Detail Data                          |\n";
+        cout << "| 0. Keluar                                         |\n";
+        cout << "+---------------------------------------------------+\n";
+        cout << "Pilih menu: ";
+        cin >> pilihan;
+
+        cin.ignore();
+
+        switch(pilihan) {
+            case 1:
+                cout << "Masukkan nama item: ";
+                getline(cin, namaItem);
+                cout << "Masukkan jumlah item: " << jumlah << endl;
+                cout << "Masukkan tipe item (Consumable/Equipment/Misc): ";
+                getline(cin, type);
+                addItem(head, tail, namaItem, jumlah, type);
+                break;
+            case 2:
+                cout << "Masukkan nama item: ";
+                getline(cin, namaItem);
+                cout << "Jumlah item: " << jumlah << endl;
+                cout << "Masukkan tipe item (Consumable/Equipment/Misc): ";
+                getline(cin, type);
+                addSpesific(head, tail, namaItem, jumlah, type);
+                break;
+            case 3:
+                deleteLast(head, tail);
+                cout << "Item terakhir telah dihapus dari inventory." << endl;
+                break;
+            case 4:
+                cout << "Masukkan nama item yang ingin digunakan: ";
+                getline(cin, namaItem);
+
+                useItem(head, tail, namaItem);
+                break;
+            case 5:
+                showInventory(head, tail);
+                break;
+            case 6:
+                showInventoryTranversal(head, tail);
+                break;
+            case 7:
+                cout << "Cari berdasarkan nama item atau id item? \n(1 untuk id, 2 untuk nama): ";
+                int subPilihan;
+                cin >> subPilihan;
+                
+                if (subPilihan == 1)
+                {   
+                    int id;
+                    cout << "Masukkan id item: ";
+                    cin >> id;
+                    showDetailData(head, tail, id, "");
+                }else if(subPilihan == 2){
+                    string nama;
+                    cout << "Masukkan nama item: ";
+                    cin.ignore();
+                    getline(cin, nama);
+                    showDetailData(head, tail, -1, nama);
+                }else{
+                    cout << "Pilihan tidak valid." << endl;
+                }
+                break;
+            case 0:
+                cout << "Keluar dari program." << endl;
+                break;
+            default:
+                cout << "Pilihan tidak valid, silakan coba lagi." << endl;
+        }
+    }
+    return 0;
+}
